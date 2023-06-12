f276d14d4fc97fefa8e40bbfb180d9ce76d5cae8
koumajos <61184328+koumajos@users.noreply.github.com>
2023-06-12 18:59:20 +0200

    Add files via upload

--- perform.sh
+++ perform.sh
@@ -42,48 +42,43 @@ done
 echo -e "${GREEN}Load arguments"
 echo "  OK"
 
-for per_level in "0.9"  "0.99" "0.999"
+
+for SEC in  86400 43200 21600 14400 7200 3600 1800 900 600
 do
-    for sig_level in "0.1" "0.01" "0.001"
-    do
-        for SEC in  86400 43200 21600 14400 7200 3600 1800 900 600
-        do
-            echo -e "${YELLOW}$SEC "
-            echo -e "${NC}" 
-            if [[ $REMOVE == "True" ]]
-            then
-                if [[ -f "time_series/design.time_series.$SEC.csv" ]]
-                then
-                    rm "time_series/design.time_series.$SEC.csv"
-                fi
-                if [[ -f "time_series/evaluation.time_series.$SEC.csv" ]]
-                then
-                
-                    rm "time_series/evaluation.time_series.$SEC.csv"
-                fi
-            fi
-            
-            if [[ -f "time_series/evaluation.time_series.$SEC.csv" ]]
-            then
-                echo -e "${GREEN} time_series/design.time_series.$SEC.csv etxts, skipp creating time series....."
-                echo -e "${NC}" 
-            else
-                echo -e "${GREEN}  ./create_time_series_from_flow.sh  -C $SEC -w time_series/design.time_series.$SEC.csv -c . -f original_dataset/design.csv -s ....."
-                echo -e "${NC}" 
-                time ./create_time_series_from_flow.sh  -C $SEC -w "time_series/design.time_series.$SEC.csv" -c "." -f "original_dataset/design.csv" -s "ipaddr SRC_IP" -d "ipaddr DST_IP" -S "uint16 SRC_PORT" -D "uint16 DST_PORT" -p "uint32 PACKETS" -P "uint32 PACKETS_REV" -b "uint64 BYTES" -B "uint64 BYTES_REV" -t "time TIME_FIRST" -T "time TIME_LAST" -l "string LABEL" -F "%Y-%m-%dT%H:%M:%S.%f"  
-            fi
-            
-            if [[ -f "time_series/evaluation.time_series.$SEC.csv" ]]
-            then
-                echo -e "${GREEN}  time_series/evaluation.time_series.$SEC.csv etxts, skipp creating time series....."
-                echo -e "${NC}" 
-            else
-                echo -e "${GREEN}  ./create_time_series_from_flow.sh  -C $SEC -w time_series/evaluation.time_series.$SEC.csv -c . -f original_dataset/evaluation.csv -s ....."
-                echo -e "${NC}" 
-                time ./create_time_series_from_flow.sh  -C $SEC -w "time_series/evaluation.time_series.$SEC.csv" -c "." -f "original_dataset/evaluation.csv" -s "ipaddr SRC_IP" -d "ipaddr DST_IP" -S "uint16 SRC_PORT" -D "uint16 DST_PORT" -p "uint32 PACKETS" -P "uint32 PACKETS_REV" -b "uint64 BYTES" -B "uint64 BYTES_REV" -t "time TIME_FIRST" -T "time TIME_LAST" -l "string LABEL" -F "%Y-%m-%dT%H:%M:%S.%f"  
-            fi
-        done
-    done
+    echo -e "${YELLOW}$SEC "
+    echo -e "${NC}" 
+    if [[ $REMOVE == "True" ]]
+    then
+        if [[ -f "time_series/design.time_series.$SEC.csv" ]]
+        then
+            rm "time_series/design.time_series.$SEC.csv"
+        fi
+        if [[ -f "time_series/evaluation.time_series.$SEC.csv" ]]
+        then
+        
+            rm "time_series/evaluation.time_series.$SEC.csv"
+        fi
+    fi
+    
+    if [[ -f "time_series/evaluation.time_series.$SEC.csv" ]]
+    then
+        echo -e "${GREEN} time_series/design.time_series.$SEC.csv etxts, skipp creating time series....."
+        echo -e "${NC}" 
+    else
+        echo -e "${GREEN}  ./create_time_series_from_flow.sh  -C $SEC -w time_series/design.time_series.$SEC.csv -c . -f original_dataset/design.csv -s ....."
+        echo -e "${NC}" 
+        time ./create_time_series_from_flow.sh  -C $SEC -w "time_series/design.time_series.$SEC.csv" -c "." -f "original_dataset/design.csv" -s "ipaddr SRC_IP" -d "ipaddr DST_IP" -S "uint16 SRC_PORT" -D "uint16 DST_PORT" -p "uint32 PACKETS" -P "uint32 PACKETS_REV" -b "uint64 BYTES" -B "uint64 BYTES_REV" -t "time TIME_FIRST" -T "time TIME_LAST" -l "string LABEL" -F "%Y-%m-%dT%H:%M:%S.%f"  
+    fi
+    
+    if [[ -f "time_series/evaluation.time_series.$SEC.csv" ]]
+    then
+        echo -e "${GREEN}  time_series/evaluation.time_series.$SEC.csv etxts, skipp creating time series....."
+        echo -e "${NC}" 
+    else
+        echo -e "${GREEN}  ./create_time_series_from_flow.sh  -C $SEC -w time_series/evaluation.time_series.$SEC.csv -c . -f original_dataset/evaluation.csv -s ....."
+        echo -e "${NC}" 
+        time ./create_time_series_from_flow.sh  -C $SEC -w "time_series/evaluation.time_series.$SEC.csv" -c "." -f "original_dataset/evaluation.csv" -s "ipaddr SRC_IP" -d "ipaddr DST_IP" -S "uint16 SRC_PORT" -D "uint16 DST_PORT" -p "uint32 PACKETS" -P "uint32 PACKETS_REV" -b "uint64 BYTES" -B "uint64 BYTES_REV" -t "time TIME_FIRST" -T "time TIME_LAST" -l "string LABEL" -F "%Y-%m-%dT%H:%M:%S.%f"  
+    fi
 done
 
 time ./merge_ts.py
